[env] implemented general local unitary operator [penny] implemented general multiplayer game in pennylane [qmain] stabilized training process [quantum] added rotation operators + fixed tens_pow

qmain.py
@@ -1,7 +1,8 @@
 # py
 from math import pi
 from itertools import chain
-from typing import Optional, Tuple
+from typing import Optional, Tuple, Callable, Dict
+from random import uniform
 
 # nn & rl
 from torch import tensor, Tensor, cat, kron, full, real, zeros, manual_seed
@@ -10,9 +11,10 @@ from torch import float32, complex64
 from torch.nn import Module, Linear
 from torch.nn.init import kaiming_normal_
 from torch.optim import Adam, Optimizer
+from bayes_opt import BayesianOptimization, UtilityFunction
 
 # lib
-from quantum import QuantumSystem, Ops, Operator, J
+from quantum import QuantumSystem, Ops, Operator
 
 
 def rotation_operator(params: Tensor) -> Operator:
@@ -113,7 +115,7 @@ class ComplexNetwork(Module):
         Initializes weights using the kaiming-normal distribution and sets weights to zero.
         """
         if isinstance(m, Linear):
-            manual_seed(2000)  # ensures reproducibility
+            # manual_seed(2000)  # ensures reproducibility
             kaiming_normal_(m.weight)
             m.bias.data.fill_(0.)
 
@@ -132,10 +134,11 @@ def main():
     env: Env = Env()
     alice: Module = ComplexNetwork()
     bob: Module = ComplexNetwork()
-    ac_optimizer: Optimizer = Adam(params=chain(alice.parameters(), bob.parameters()))
+    al_op = Adam(params=alice.parameters())
+    bo_op = Adam(params=bob.parameters())
 
     # loop over episodes
-    for step in range(1000):
+    for step in range(10000):
         # get state from environment
         state = env.reset()
 
@@ -147,14 +150,30 @@ def main():
         q_alice, q_bob = env.step(ac_alice, ac_bob)
 
         # define loss
-        alice_loss = -q_alice
-        bob_loss = -q_bob
-        pg_loss = alice_loss + bob_loss
+        alice_loss: Tensor = -q_alice
 
-        # update agents
-        ac_optimizer.zero_grad()
-        pg_loss.backward()
-        ac_optimizer.step()
+        # optimize alice
+        al_op.zero_grad()
+        alice_loss.backward()
+        al_op.step()
+
+        # get state from environment
+        state = env.reset()
+
+        # compute actions
+        ac_bob = bob(state)
+        ac_alice = alice(state)
+
+        # compute q-values
+        q_alice, q_bob = env.step(ac_alice, ac_bob)
+
+        # define loss
+        bob_loss: Tensor = -q_bob
+
+        # optimize bob
+        bo_op.zero_grad()
+        bob_loss.backward()
+        bo_op.step()
 
         if step % 200 == 0:
             print(q_alice.item(), q_bob.item())

quantum.py
@@ -1,11 +1,12 @@
-from torch import tensor, Tensor, zeros, complex64, eye, sqrt, kron, int64
+# nn & rl
+from torch import tensor, Tensor, zeros, complex64, eye, sqrt, kron, int64, matrix_exp
 from torch.distributions import Multinomial, Distribution
 from torch.nn.functional import one_hot
 
 
 def tens_pow(n: int, t: Tensor) -> Tensor:
     out: Tensor = t
-    for i in range(n):
+    for i in range(1, n):
         out = kron(out, t)
     return out
 
@@ -101,6 +102,39 @@ class PauliZ(Operator):
         super(PauliZ, self).__init__(mat=pauli_z)
 
 
+class RotX(Operator):
+
+    def __init__(self, rotx: Tensor = PauliX().mat):
+        super(RotX, self).__init__(mat=rotx)
+
+    @classmethod
+    def inject(cls, theta: Tensor) -> Operator:
+        rotx = matrix_exp(1j / 2 * theta * PauliX().mat)
+        return cls(rotx=rotx)
+
+
+class RotY(Operator):
+
+    def __init__(self, rotx: Tensor = PauliY().mat):
+        super(RotY, self).__init__(mat=rotx)
+
+    @classmethod
+    def inject(cls, theta: Tensor) -> Operator:
+        rotx = matrix_exp(1j / 2 * theta * PauliY().mat)
+        return cls(rotx=rotx)
+
+
+class RotZ(Operator):
+
+    def __init__(self, rotx: Tensor = PauliZ().mat):
+        super(RotZ, self).__init__(mat=rotx)
+
+    @classmethod
+    def inject(cls, theta: Tensor) -> Operator:
+        rotx = matrix_exp(1j / 2 * theta * PauliZ().mat)
+        return cls(rotx=rotx)
+
+
 class H(Operator):
 
     def __init__(self):
@@ -139,6 +173,9 @@ class Ops:
     _H: H
     _CNOT: CNOT
     _J: J
+    _RotX: RotX
+    _RotY: RotY
+    _RotZ: RotZ
 
     def __init__(self):
         self._I = I()
@@ -148,6 +185,9 @@ class Ops:
         self._H = H()
         self._CNOT = CNOT()
         self._J = J()
+        self._RotX = RotX()
+        self._RotY = RotY()
+        self._RotZ = RotZ()
 
     @property
     def I(self) -> I:
@@ -176,6 +216,18 @@ class Ops:
     @property
     def J(self) -> J:
         return self._J
+
+    @property
+    def RotX(self) -> RotX:
+        return self._RotX
+
+    @property
+    def RotY(self) -> RotY:
+        return self._RotY
+
+    @property
+    def RotZ(self) -> RotZ:
+        return self._RotZ
 
 
 if __name__ == '__main__':